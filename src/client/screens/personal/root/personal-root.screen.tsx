71c1fe5df13de9563769d7b7ad5ef5efbafb024a
yarastqt <yarastqt@gmail.com>
2023-07-29 02:36:54 +0400

    wip

--- src/client/screens/personal/root/personal-root.screen.tsx
+++ src/client/screens/personal/root/personal-root.screen.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
 
+import ThemekitImage from '@client/shared/assets/themekit.png';
+
 export const PersonalRootScreen: FC = () => {
   return (
     <div>
       <Helmet>
         <title>Personal page</title>
       </Helmet>
+      <img src={ThemekitImage} width={400} />
       personal root page
     </div>
   );
