d3dd241df474cd423b49292d49e1a7bf37fe6fbb
Edward Gigolaev <49155506+movpushmov@users.noreply.github.com>
2024-02-22 16:30:23 +0300

    fix: add dev and production plugins logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, IndexHtmlTransformResult } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { resolve } from 'node:path';
 import legacy from '@vitejs/plugin-legacy';
 
 import react from '@vitejs/plugin-react';
 import { babel } from '@rollup/plugin-babel';
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
-export default defineConfig(({ ssrBuild }) => ({
-  build: {
-    emptyOutDir: false,
-    outDir: ssrBuild ? './build/server' : './build/client',
+export default defineConfig(({ ssrBuild }) => {
+  const isDevMode = process.env.NODE_ENV === 'development';
 
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-      },
-      // output: {
-      //   assetFileNames:
-      //     'https://yastatic.net/s3/yandex-id-static/[name]-[hash].[ext]',
-      //   chunkFileNames:
-      //     'https://yastatic.net/s3/yandex-id-static/[name]-[hash].js',
-      //   entryFileNames:
-      //     'https://yastatic.net/s3/yandex-id-static/[name]-[hash].js',
-      // },
-    },
-  },
-  plugins: [
+  const plugins = [
     legacy({
       targets: ['defaults', 'not IE 11'],
     }),
@@ -35,5 +20,52 @@ export default defineConfig(({ ssrBuild }) => ({
     }),
     react(),
     tsconfigPaths(),
-  ],
-}));
+  ];
+
+  if (!isDevMode) {
+    plugins.push(
+      injectExternalReactWithDom(),
+      viteExternalsPlugin({ react: 'React', 'react-dom': 'ReactDOM' }),
+    )
+  }
+
+  return {
+    build: {
+      emptyOutDir: false,
+      outDir: ssrBuild ? './build/server' : './build/client',
+
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+        },
+        // output: {
+        //   assetFileNames:
+        //     'https://yastatic.net/s3/yandex-id-static/[name]-[hash].[ext]',
+        //   chunkFileNames:
+        //     'https://yastatic.net/s3/yandex-id-static/[name]-[hash].js',
+        //   entryFileNames:
+        //     'https://yastatic.net/s3/yandex-id-static/[name]-[hash].js',
+        // },
+      },
+    },
+    plugins,
+  }
+});
+
+function injectExternalReactWithDom() {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: (html: string): IndexHtmlTransformResult => ({
+      html,
+      tags: [
+        {
+          tag: 'script',
+          attrs: {
+            src: 'https://yastatic.net/react/18.2.0/react-with-dom.min.js',
+          },
+          injectTo: 'head-prepend',
+        },
+      ],
+    }),
+  };
+}
