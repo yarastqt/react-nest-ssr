4eb626d09a745e9dad3e6e141c26c2047dcfdf21
Edward Gigolaev <49155506+movpushmov@users.noreply.github.com>
2024-02-23 13:16:27 +0300

    fix: 404 page render

--- src/server/app.filter.ts
+++ src/server/app.filter.ts
@@ -3,16 +3,40 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
-} from '@nestjs/common';
+} from '@nestjs/common'
 import { Request, Response } from 'express';
+import { RendererService } from '@server/infrastructure/renderer/renderer.service'
+
+@Catch()
+export class ServeStaticExceptionFilter implements ExceptionFilter {
+  constructor(private rendererService: RendererService) {}
+
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    if (exception.code === "ENOENT") {
+      void this.rendererService.render({ response, request });
+      return;
+    }
+  }
+}
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private rendererService: RendererService) {}
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+
+    if (status === 404) {
+      void this.rendererService.render({ response, request });
+      return;
+    }
 
     response.status(status).json({
       statusCode: status,
--- src/server/infrastructure/infrastructure.module.ts
+++ src/server/infrastructure/infrastructure.module.ts
@@ -1,18 +1,26 @@
 import { resolve } from 'node:path';
 
-import { Module } from '@nestjs/common';
+import { Module, ModuleMetadata } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { RendererModule } from './renderer';
 import { ViteModule } from './vite';
+import { isProduction } from '@shared/lib/environment'
 
-@Module({
-  imports: [
-    ViteModule,
-    RendererModule,
+const imports: ModuleMetadata['imports'] = [
+  ViteModule,
+  RendererModule,
+]
+
+if (isProduction) {
+  imports.push(
     ServeStaticModule.forRoot({
       rootPath: resolve(process.cwd(), 'build/client'),
     }),
-  ],
+  )
+}
+
+@Module({
+  imports,
 })
 export class InfrastructureModule {}
